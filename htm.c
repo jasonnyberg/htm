a293ad2d91ee6362fd332bc4ef0c5040014fa864
Jason Nyberg <jasonnyberg@gmail.com>
2011-12-15 04:13:47 -0500

    add numenta license header to htm.c

--- htm.c
+++ htm.c
@@ -1,3 +1,14 @@
+/* htm.c: part of an implementation of the Numenta Cortical Learning Algorithm.
+ * Copyright (c) 2011 Jason Nyberg <jasonnyberg@gmail.com>
+ *  
+ * This software is released under the Numenta License for
+ * Non-Commercial Use. You should have received a copy of the Numenta
+ * License for Non-Commercial Use with this software (usually
+ * contained in a file called LICENSE.TXT). If you don't, you can
+ * download it from 
+ * http://numenta.com/about-numenta/Numenta_Non-Commercial_License.txt.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
